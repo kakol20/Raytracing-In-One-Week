cfd7de0ad394efef9a3e8d210d3c44f89d1d1c48
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2021-12-10 06:04:45 +0000

    Add Image.h

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
+#include "Image.h"
+
+bool Image::PrintToConsole = true;
+
 int main() {
+	Image test(256, 256, 3);
+
+	for (int x = 0; x <= 255; x++) {
+		for (int y = 0; y <= 255; y++) {
+			int index = test.GetIndex(x, y);
+
+			test.SetData(index + 0, (uint8_t)x);
+			test.SetData(index + 1, (uint8_t)y);
+		}
+	}
+
+	test.Write("images/test.png");
+
 	return 0;
 }
