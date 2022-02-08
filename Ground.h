dbd58a3640034cfefe62f527aaf4cf8485bfd81b
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2022-02-08 22:15:22 +0000

    Rewrite start from scratch

--- Ground.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Object.h"
-
-class Ground : public Object {
-public:
-	Ground(const float height, Material* mat);
-
-	virtual bool Hit(Ray& ray, const float t_min, const float t_max, HitRec& rec);
-};
