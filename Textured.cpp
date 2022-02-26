1b697530f070cbd3d034b82d0eac34c04eb01be2
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2022-02-26 09:25:48 +0000

    Fix dark spots in textured emission

--- Textured.cpp
+++ Textured.cpp
@@ -34,11 +34,13 @@ bool Textured::Emission(HitRec& rec, Vector3D& emission) {
 	m_albedoTexture->BiLerp(uv.GetX(), uv.GetY(), r, g, b);
 	Vector3D albedo(r, g, b);
 	albedo /= 255.f;
+	float emissionModified = l_emission * m_emissionStrength;
+	l_emission = round(l_emission);
 
 	bool emissionRand = Random::RandFloat() <= l_emission;
 
 	if (emissionRand) {
-		emission = albedo * m_emissionStrength;
+		emission = albedo * emissionModified;
 		return true;
 	}
 	else {
@@ -62,6 +64,8 @@ bool Textured::Scatter(Ray& rayIn, HitRec& rec, Vector3D& attentuation, Ray& sca
 		metalness = 0.f;
 		emission = 0.f;
 	}
+
+	emission = round(emission);
 
 	// ----- ALBEDO -----
 	uv = (rec.GetUV() + m_uvOffset) * Vector3D((float)m_albedoTexture->GetWidth(), (float)m_albedoTexture->GetHeight(), 0.f);
