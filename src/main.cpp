451e08ab74424ae09df392604df41ad4d17f9ab7
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2022-08-28 14:43:24 +0100

    Add SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <string>
+
+#include <SFML/Main.hpp>
+#include <SFML/Graphics.hpp>
 
 #ifdef _DEBUG
 int main() {
@@ -6,8 +10,27 @@ int main() {
 int WinMain() {
 #endif // _DEBUG
 
-	std::cout << "Hello World\n";
-	std::cin.ignore();
+	const unsigned int width = 1280;
+	const unsigned int height = 720;
+	const std::string windowName = "Raytracing";
+	const sf::Uint32 style = sf::Style::Titlebar | sf::Style::Close;
+
+	sf::RenderWindow window(sf::VideoMode(width, height), windowName, style);
+
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
 
 	return  0;
 }
