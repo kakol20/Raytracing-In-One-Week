b73ed793a6ea6391c66432e478e4db6500dba53e
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2023-09-06 21:34:08 +0100

    Recreate metal material

--- src/raytracing/materials/Dielectric.cpp
+++ src/raytracing/materials/Dielectric.cpp
@@ -2,54 +2,16 @@
 
 Dielectric::Dielectric(const Vector3D& albedo, const Float roughness, const Float ior) {
 	m_albedo = albedo;
-	m_roughness = Float::Pow(roughness, 2.2);
+	m_roughness = roughness * roughness;
 	m_ior = ior;
 }
 
 void Dielectric::Scatter(Ray& rayIn, HitRec& rec, Vector3D& attentuation, Ray& scattered, Vector3D& normal, bool& absorb, bool& transparent, bool& emission) {
-	//normal = rec.GetNormal();
-	//const Vector3D incoming = -rayIn.GetDir();
-	//const Vector3D dir = rayIn.GetDir();
-
-	//Float roughnessRand = Random::RandomFloat();
-
-
-	//Float fresnelRand = Random::RandomFloat();
-
-	//// diffuse part
-
-	//Vector3D diffuseCol = m_albedo;
-	//Vector3D diffuseScatter = Vector3D::RandomInUnitSphere() + normal;
-	//diffuseScatter.Normalize();
-
-	//// white glossy part
-	//
-	//Vector3D whiteGlossCol = Vector3D::One;
-	//Vector3D whiteGlossScatter = Vector3D::Reflect(dir, normal) + ((Vector3D::RandomInUnitSphere() + normal) * m_roughness);
-
-	//// mix them together
-	//
-	//Vector3D totalColor = Vector3D::RandomMix(diffuseCol, whiteGlossCol, fresnel, fresnelRand);
-	//Vector3D totalScatter = Vector3D::RandomMix(diffuseScatter, whiteGlossScatter, fresnel, fresnelRand);
-	//totalScatter.Normalize();
-
-	//scattered = Ray(rec.GetPoint(), totalScatter);
-	//attentuation = totalColor;
-	//absorb = false;
-	//transparent = false;
-	//emission = false;
-
-	//Vector3D unitDir = rayIn.GetDir();
-
 	Vector3D incoming = -rayIn.GetDir();
 
 	normal = rec.GetNormal();
 
 	// ----- FRESNEL -----
-	
-	//const Float refractionRatio = rec.GetFrontFace() ? 1 / m_ior : m_ior;
-	//Vector3D fresnelNormal = Vector3D::RandomMix(normal, incoming, m_roughness, roughnessRand);
-	//Float fresnel = Fresnel(incoming, fresnelNormal, refractionRatio);
 
 	Float roughnessRand = Random::RandomFloat();
 
