eb74cf88f373b9e623b7abef7669f9490dddf934
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2024-09-27 16:00:42 +0100

    .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 ﻿#include <iostream>
 
 int main(int argc, char* argv[]) {
-  std::cout << "Hello World\n";
+	std::cout << "Hello World\n";
 
-  std::cout << "Press enter to exit...\n";
-  std::cin.ignore();
-  return 0;
+	std::cout << "Press enter to exit...\n";
+	std::cin.ignore();
+	return 0;
 }
